a1eb129e930656be3c8d6cd59a5f4d59f25e52e9
gyomdyung <seastory624@naver.com>
2023-02-01 23:37:07 +0900

    Feat: Category 엔티티 정의

--- src/api/categories/categories.module.ts
+++ src/api/categories/categories.module.ts
@@ -1,9 +1,12 @@
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { Module } from '@nestjs/common';
 import { CategoriesService } from './categories.service';
 import { CategoriesController } from './categories.controller';
+import { Category } from './entities/category.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Category])],
   controllers: [CategoriesController],
-  providers: [CategoriesService]
+  providers: [CategoriesService],
 })
 export class CategoriesModule {}
--- src/api/categories/entities/category.entity.ts
+++ src/api/categories/entities/category.entity.ts
@@ -1,1 +1,18 @@
-export class Category {}
+import { CommonEntity } from 'src/common/entities/common.entity';
+import { Entity, Column } from 'typeorm';
+
+@Entity()
+export class Category extends CommonEntity {
+  @Column({ type: 'varchar', comment: '카테고리명', nullable: false })
+  name: string;
+
+  @Column({
+    type: 'varchar',
+    comment: '학명(Thuja occidentalis)',
+    nullable: false,
+  })
+  scientific: string;
+
+  @Column({ type: 'varchar', comment: '과명(측백나무과)', nullable: false })
+  department: string;
+}
