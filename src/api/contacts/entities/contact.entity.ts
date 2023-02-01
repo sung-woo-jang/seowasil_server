63a9e3436113fafe4569f3bb97fb3c825ec30a9b
gyomdyung <seastory624@naver.com>
2023-02-01 23:31:30 +0900

    Feat: Contact 엔티티 정의

--- src/api/contacts/entities/contact.entity.ts
+++ src/api/contacts/entities/contact.entity.ts
@@ -1,1 +1,25 @@
-export class Contact {}
+import { CommonEntity } from 'src/common/entities/common.entity';
+import { Entity, Column } from 'typeorm';
+
+@Entity()
+export class Contact extends CommonEntity {
+  @Column({ type: 'varchar', comment: '문의 제목', nullable: false })
+  title: string;
+
+  @Column({ type: 'varchar', comment: '문의 내용', nullable: false })
+  description: string;
+
+  @Column({ type: 'varchar', comment: '글쓴이', nullable: false })
+  name: string;
+
+  @Column({ type: 'varchar', comment: '문의글 비밀번호', nullable: false })
+  password: string;
+
+  @Column({
+    type: 'varchar',
+    comment: '문의 카테고리',
+    nullable: false,
+    default: '.',
+  })
+  category: string;
+}
--- src/api/notices/entities/notice.entity.ts
+++ src/api/notices/entities/notice.entity.ts
@@ -3,23 +3,9 @@ import { CommonEntity } from 'src/common/entities/common.entity';
 
 @Entity()
 export class Notice extends CommonEntity {
-  @Column({ type: 'varchar', comment: '문의 제목', nullable: false })
+  @Column({ type: 'varchar', comment: '공지 제목', nullable: false })
   title: string;
 
-  @Column({ type: 'varchar', comment: '문의 내용', nullable: false })
+  @Column({ type: 'varchar', comment: '공지 내용', nullable: false })
   description: string;
-
-  @Column({ type: 'varchar', comment: '글쓴이', nullable: false })
-  name: string;
-
-  @Column({ type: 'varchar', comment: '문의글 비밀번호', nullable: false })
-  password: string;
-
-  @Column({
-    type: 'varchar',
-    comment: '문의 카테고리',
-    nullable: false,
-    default: '.',
-  })
-  category: string;
 }
