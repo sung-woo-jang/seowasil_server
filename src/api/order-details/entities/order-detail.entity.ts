b745637e277809e660a1570cbc4b859fe4cee938
gyomdyung <seastory624@naver.com>
2023-02-01 23:16:30 +0900

    Feat: OrderDetail 엔티티 정의

--- src/api/order-details/entities/order-detail.entity.ts
+++ src/api/order-details/entities/order-detail.entity.ts
@@ -1,1 +1,15 @@
-export class OrderDetail {}
+import { CommonEntity } from 'src/common/entities/common.entity';
+import { Entity, Column } from 'typeorm';
+
+@Entity({ name: 'OrderDetail' })
+export class OrderDetail extends CommonEntity {
+  @Column({ type: 'integer', comment: '주문수량', nullable: false })
+  amount: number;
+
+  @Column({
+    type: 'integer',
+    comment: '결제 금액(주문수량 * 제품가격)',
+    nullable: false,
+  })
+  price: number;
+}
--- src/api/order-details/order-details.module.ts
+++ src/api/order-details/order-details.module.ts
@@ -1,9 +1,12 @@
+import { OrderDetail } from './entities/order-detail.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { Module } from '@nestjs/common';
 import { OrderDetailsService } from './order-details.service';
 import { OrderDetailsController } from './order-details.controller';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([OrderDetail])],
   controllers: [OrderDetailsController],
-  providers: [OrderDetailsService]
+  providers: [OrderDetailsService],
 })
 export class OrderDetailsModule {}
