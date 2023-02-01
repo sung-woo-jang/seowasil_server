39da9f3f2f340f8906d0a8e603733dff0bd2c16f
gyomdyung <seastory624@gmail.com>
2023-02-01 23:40:25 +0900

    Feat: Cart 엔티티 정의

--- src/api/carts/carts.module.ts
+++ src/api/carts/carts.module.ts
@@ -1,9 +1,12 @@
+import { Cart } from './entities/cart.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { Module } from '@nestjs/common';
 import { CartsService } from './carts.service';
 import { CartsController } from './carts.controller';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Cart])],
   controllers: [CartsController],
-  providers: [CartsService]
+  providers: [CartsService],
 })
 export class CartsModule {}
--- src/api/carts/entities/cart.entity.ts
+++ src/api/carts/entities/cart.entity.ts
@@ -1,1 +1,8 @@
-export class Cart {}
+import { CommonEntity } from 'src/common/entities/common.entity';
+import { Entity, Column } from 'typeorm';
+
+@Entity()
+export class Cart extends CommonEntity {
+  @Column({ type: 'integer', comment: '주문 수량', nullable: false })
+  amount: number;
+}
