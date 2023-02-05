4cdd632ffa355fea78b1a15958a9c838cb2f9cd8
gyomdyung <seastory624@gmail.com>
2023-02-05 13:30:39 +0900

    Docs: 문의사항 API 의사코드

--- src/api/contacts/contacts.controller.ts
+++ src/api/contacts/contacts.controller.ts
@@ -1,7 +1,61 @@
-import { Controller } from '@nestjs/common';
+import { UpdateContactDto } from './dto/update-contact.dto';
+import { CreateContactDto } from './dto/create-contact.dto';
+import {
+  Controller,
+  Post,
+  Body,
+  Get,
+  Patch,
+  Delete,
+  Param,
+  ParseIntPipe,
+} from '@nestjs/common';
 import { ContactsService } from './contacts.service';
 
 @Controller('contacts')
 export class ContactsController {
   constructor(private readonly contactsService: ContactsService) {}
+
+  /**
+   * @description 문의사항 등록
+   */
+  @Post()
+  createContact(@Body() createContactDto: CreateContactDto) {
+    return this.contactsService.createContact(createContactDto);
+  }
+
+  /**
+   * @description 문의사항 목록 가져오기
+   */
+  @Get()
+  getContactList() {
+    return this.contactsService.getContactList();
+  }
+
+  /**
+   * @description 문의사항 목록 가져오기
+   */
+  @Get()
+  getContact() {
+    return this.contactsService.getContact();
+  }
+
+  /**
+   * @description 문의사항 정보 수정
+   */
+  @Patch('/:id')
+  updateContact(
+    @Body() updateContactDto: UpdateContactDto,
+    @Param('id', ParseIntPipe) id: number,
+  ) {
+    return this.contactsService.updateContact(updateContactDto, id);
+  }
+
+  /**
+   * @description 문의사항 삭제
+   */
+  @Delete('/:id')
+  deleteContact(@Param('id', ParseIntPipe) id: number) {
+    return this.contactsService.deleteContact(id);
+  }
 }
--- src/api/contacts/contacts.service.ts
+++ src/api/contacts/contacts.service.ts
@@ -1,6 +1,8 @@
 import { ContactsRepository } from './contacts.repository';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Injectable } from '@nestjs/common';
+import { UpdateContactDto } from './dto/update-contact.dto';
+import { CreateContactDto } from './dto/create-contact.dto';
 
 @Injectable()
 export class ContactsService {
@@ -8,4 +10,24 @@ export class ContactsService {
     @InjectRepository(ContactsRepository)
     private contactsRepository: ContactsRepository,
   ) {}
+
+  createContact(createContactDto: CreateContactDto) {
+    throw new Error('Method not implemented.');
+  }
+
+  getContactList() {
+    throw new Error('Method not implemented.');
+  }
+
+  getContact() {
+    throw new Error('Method not implemented.');
+  }
+
+  updateContact(updateContactDto: UpdateContactDto, id: number) {
+    throw new Error('Method not implemented.');
+  }
+
+  deleteContact(id: number) {
+    throw new Error('Method not implemented.');
+  }
 }
