0c42b39bacf32894780587c145239ab3d0cf1b66
gyomdyung <seastory624@gmail.com>
2023-02-26 01:24:15 +0900

    Revert "Refactor: 카테고리별 상품 불러오기 응답값 변경"
    This reverts commit c742ccf26a0e74d7d221a9a841146ee4f27c3d85.

--- src/api/categories/categories.service.ts
+++ src/api/categories/categories.service.ts
@@ -28,14 +28,11 @@ export class CategoriesService {
     const result = await this.categoriesRepository
       .createQueryBuilder('category')
       .leftJoinAndSelect('category.product', 'product')
-      .leftJoinAndSelect('product.productImageUrl', 'productImageUrl')
       .select([
         'category.id',
         'category.name',
-        'product.id',
         'product.title',
         'product.description',
-        'productImageUrl.storedFileName',
       ])
       .where('category.id = :id', { id })
       .getMany();
