c278fd01c87f4fe9015b4e7cf996af3c42b78519
gyomdyung <54757435+sung-woo-jang@users.noreply.github.com>
2023-10-04 13:01:31 +0900

    eslint 수정

--- src/api/contact-category/dto/update-contact-category.dto.ts
+++ src/api/contact-category/dto/update-contact-category.dto.ts
@@ -1,4 +1,6 @@
 import { PartialType } from '@nestjs/swagger';
 import { CreateContactCategoryDto } from './create-contact-category.dto';
 
-export class UpdateContactCategoryDto extends PartialType(CreateContactCategoryDto) {}
+export class UpdateContactCategoryDto extends PartialType(
+  CreateContactCategoryDto,
+) {}
--- src/api/deliver-address/dto/update-deliver-address.dto.ts
+++ src/api/deliver-address/dto/update-deliver-address.dto.ts
@@ -1,4 +1,6 @@
 import { PartialType } from '@nestjs/swagger';
 import { CreateDeliverAddressDto } from './create-deliver-address.dto';
 
-export class UpdateDeliverAddressDto extends PartialType(CreateDeliverAddressDto) {}
+export class UpdateDeliverAddressDto extends PartialType(
+  CreateDeliverAddressDto,
+) {}
