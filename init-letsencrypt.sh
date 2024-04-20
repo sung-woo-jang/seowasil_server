2f681386370f3da6cd929c4794b02a2568ce2df5
gyomdyung <54757435+sung-woo-jang@users.noreply.github.com>
2024-04-20 15:57:07 +0900

    docker-compose 수정

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(www.seowasil.store)
+domains=(api.seowasil.store)
 rsa_key_size=4096
 data_path="./prod/data/certbot"
 email="seastory624@gmail.com" # Adding a valid address is strongly recommended
